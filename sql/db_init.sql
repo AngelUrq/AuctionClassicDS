4e1e8866270dcd81150bdcb1f3699ba0adf3549f
AngelUrq <angel4441916@gmail.com>
2021-09-18 15:33:16 -0400

    SQL script to create DB added

---
+++ sql/db_init.sql
@@ -0,0 +1,41 @@
+USE [AuctionClassicDB];
+
+DROP TABLE IF EXISTS [Auction];
+DROP TABLE IF EXISTS [RecordedAuction];
+DROP TABLE IF EXISTS [Item];
+
+CREATE TABLE [Auction] (
+    [Id] BIGINT PRIMARY KEY,
+    [ItemId] INT,
+    [BidGold] INT,
+    [BidSilver] INT,
+    [BuyoutGold] INT,
+    [BuyoutSilver] INT,
+    [Quantity] INT,
+    [TimeLeft] VARCHAR(10),
+    [Rand] INT,
+    [Seed] INT,
+    [LastTimeSeen] DATETIME
+);
+
+CREATE TABLE [RecordedAuction] (
+    [AuctionId] BIGINT,
+    [RecordedTime] DATETIME
+    CONSTRAINT PK_RecordedAuction PRIMARY KEY NONCLUSTERED ([AuctionId], [RecordedTime])
+);
+
+CREATE TABLE [Item] (
+    [Id] INT PRIMARY KEY,
+    [Name] NVARCHAR(70),
+    [Quality] NVARCHAR(40),
+    [Level] INT,
+    [RequiredLevel] INT,
+    [ItemClass] NVARCHAR(40),
+    [ItemSubClass] NVARCHAR(40),
+    [PurchasePrice] BIGINT,
+    [SellPrice] BIGINT,
+    [MaxCount] INT,
+    [IsEquippable] BIT,
+    [IsStackable] BIT,
+    [PurchaseQuantity] INT
+);
